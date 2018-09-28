1ed6dc18a348b1df3f00cf3e40371eecb663e63c
juinsRP <43670943+juinsRP@users.noreply.github.com>
2018-09-28 13:34:23 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -232,9 +232,9 @@ bot.login(process.env.token);
 
 bot.on('ready', () => {
     console.log("Бот был успешно запущен!");
-    bot.guilds.find(g => g.id == "488400983496458260").channels.find(c => c.name == "general").send(`\`Я был запущен! Версия ${version}\``)
+    bot.guilds.find(g => g.id == "490095436212862977").channels.find(c => c.name == "general").send(`\`Я был запущен! Версия ${version}\``)
     if (!hideobnova){
-        if (bot.guilds.find(g => g.id == "488400983496458260").channels.find(c => c.name == "info")) bot.guilds.find(g => g.id == "488400983496458260").channels.find(c => c.name == "updates-bot-user").send(`**DISCORD BOT UPDATE** @everyone\n\`\`\`diff
+        if (bot.guilds.find(g => g.id == "471579337779445760").channels.find(c => c.name == "info")) bot.guilds.find(g => g.id == "471579337779445760").channels.find(c => c.name == "updates-bot-user").send(`**DISCORD BOT UPDATE** @everyone\n\`\`\`diff
 Вышло обновление версии ${version}:
 - Пофиксил "undefined" в "/setadmin [USER] [LVL]"
 » Vlad_Botorov.\`\`\``).then(msgdone => {
@@ -247,7 +247,7 @@ bot.on('ready', () => {
 
 bot.on('message', async message => {
     if (message.channel.type == "dm") return // Если в ЛС, то выход.
-    if (message.guild.id != "455280266651369482" && message.guild.id != "488400983496458260" && message.guild.id != "495177551098937345") return
+    if (message.guild.id != "490095436212862977" && message.guild.id != "490095436212862977" && message.guild.id != "490095436212862977") return
     if (message.type === "PINS_ADD") if (message.channel.name == "requests-for-roles") message.delete();
     if (message.content == "/ping") return message.reply("`я онлайн.`") && console.log(`Бот ответил ${message.member.displayName}, что я онлайн.`)
     if (message.member.id == bot.user.id) return
